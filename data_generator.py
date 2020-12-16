d543e44117df8811d77b6f6d29500280089317bc
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-12-16 01:31:35 -0500

    added print statements

--- data_generator.py
+++ data_generator.py
@@ -11,6 +11,9 @@ class Data_Generator():
 
     def generate_data(self):
 
+        num_configurations_total = len(self.num_gelt_list) * len(self.num_players_list)
+        num_configurations_complete = 0
+
         for num_players in self.num_players_list:
             for num_gelt in self.num_gelt_list:
 
@@ -18,5 +21,10 @@ class Data_Generator():
                 game_lengths = self.game_simulator.get_game_lengths(self.num_simulations)
 
                 self.data.append((num_players, num_gelt, game_lengths))
+
+                num_configurations_complete += 1
+
+                if (num_configurations_complete % 10 == 0) or (num_configurations_complete == num_configurations_total):
+                    print("{}/{} Configurations Complete".format(num_configurations_complete, num_configurations_total))
 
         return self.data
